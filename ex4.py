f0474fddbf874231084b51cfe0081826bde0ed5d
Daniel Rolnik <danrol92@gmail.com>
2020-01-26 13:21:57 +0200

    added counter for classifiers

--- ex4.py
+++ ex4.py
@@ -32,10 +32,9 @@ def load_data():
     headers = ['checking_status', 'saving_status', 'credit_history', 'housing', 'job', 'property_magnitude',
                 'number_of_dependents', 'number_of_existing_credits', 'own_telephone', 'foreign_workers', 'label']
     train_data = pd.read_csv("dataset/train.txt", header=None, names=headers)
-    test_data = pd.read_csv("dataset/val.txt", header=None, names=headers)
+    headers = headers[:-1]
+    # test_data = pd.read_csv("dataset/val.txt", header=None, names=headers)
 
-    headers = ['checking_status', 'saving_status', 'credit_history', 'housing', 'job', 'property_magnitude',
-                'number_of_dependents', 'number_of_existing_credits', 'own_telephone', 'foreign_workers', 'label']
     # header_attributes = {features[0]: ['x', 'n', 'b', 'g'], features[1]: ['n', 'b', 'm', 'g', 'w'],
     #                      features[2]: ['a', 'c', 'd', 'e', 'n'], features[3]}
     get_attributes('dataset/header_attributes.txt')
@@ -56,11 +55,37 @@ def get_attributes(filename):
     header_attributes[label_header] = ['G', 'B']
 
 
-def get_header_entropy(header):
+def init_header_classifiers_counter(header):
+    global header_attributes
     output_counter_for_header_attributes = dict()
     for attribute in header_attributes[header]:
-        output_counter_for_header_attributes[attribute] = 0
-    print(output_counter_for_header_attributes)
+        output_counter_for_header_attributes[attribute] = dict()
+        for classifier in classifiers:
+            output_counter_for_header_attributes[attribute][classifier] = 0
+    log.info(f'\nheader = {header}, output_counter_for_header_attributes: \n{output_counter_for_header_attributes}')
+    return output_counter_for_header_attributes
+
+
+def count_classifiers_per_attribute(header):
+    output_counter_for_header_attributes = init_header_classifiers_counter(header)
+    for attribute, label in zip(train_data[header], labels_column):
+        attribute = str(attribute)
+        label = str(label)
+        if attribute in output_counter_for_header_attributes:
+            if label in output_counter_for_header_attributes[attribute]:
+                output_counter_for_header_attributes[attribute][label] += 1
+            else:
+                print(f'label = {label} is not in output_counter_for_header_attributes')
+        else:
+            print(f'attribute = {attribute} is not in output_counter_for_header_attributes ')
+    return output_counter_for_header_attributes
+
+
+def get_header_entropy(header):
+    output_counter_for_header_attributes = count_classifiers_per_attribute(header)
+    log.info(f'\noutput_counter_for_header_attributes in header = {header}: \n{output_counter_for_header_attributes}')
+    for attribute in header_attributes:
+        pass
 
 
 def get_gain(header):
